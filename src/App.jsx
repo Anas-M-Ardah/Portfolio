98918d4f74f1391fa3ac2fee0ad1db942889d2e1
Anas Ardah <anas.m.ardah@gmail.com>
2025-03-08 22:10:10 +0300

    testing google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,17 @@ import {Skills} from './components/Skills'
 import {Projects} from './components/Projects'
 import {Footer} from './components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactGA from 'react-ga4';
+import { useEffect } from 'react';
 
 function App() {
 
+  useEffect(() => {
+    // Initialize Google Analytics
+    ReactGA.initialize('G-7VT3WR0Y9P');
+    // Send pageview
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
 
   return (
     <>
